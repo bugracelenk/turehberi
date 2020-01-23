7407e23a186413f3796dce60a608eee8ee6dd9b4
Altuğ Buğra Çelenk <33699679+bugracelenk@users.noreply.github.com>
2020-01-23 12:14:26 +0300

    Update cities.js

--- routes/cities.js
+++ routes/cities.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const controllers = require("../controllers/cityies");
+const controllers = require("../controllers/cities");
 
 const check_auth = require("../middleware/checkAuth");
 const isAdmin = require("../middleware/isAdmin");
